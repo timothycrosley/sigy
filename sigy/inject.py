dd2709cbf8827bed428d0264fb85705c15782875
Timothy Crosley <timothy.crosley@gmail.com>
2022-12-30 22:24:32 -0800

    Add initial validation that conflicting params aren't defined, and foward along type defintiions

--- sigy/inject.py
+++ sigy/inject.py
@@ -1,10 +1,12 @@
 from functools import wraps
-from typing import Callable, Any
+from typing import Any, Callable
+
 from sigy import introspect
+
 
 def _generate_accepted_kwargs(function, kwargs) -> dict[str, Any]:
     """Dynamically creates a function that when called with dictionary of arguments will produce a kwarg that's
-       compatible with the supplied function
+    compatible with the supplied function
     """
     if hasattr(function, "__code__") and introspect.takes_kwargs(function):
         function_takes_kwargs = True
@@ -22,12 +24,32 @@ def _generate_accepted_kwargs(function, kwargs) -> dict[str, Any]:
 
 def inject(**override_callbacks: Callable):
     def wrapper(function):
+        type_overrides: dict[str, Any] = {}
+
+        for name, callback in override_callbacks.items():
+            for param_name, type_override in callback.__annotations__.items():
+                if param_name in ("return", ):
+                    continue
+                
+                existing_type = type_overrides.get(name, function.__annotations__.get(param_name))
+                if type_override:
+                    if (
+                        existing_type
+                        and type_override != existing_type
+                    ):
+                        raise TypeError(
+                            f"Type mismatch introduced by sigy injected function {callback} "
+                            f"parameter {param_name} requiring type {type_override} existing: {existing_type}."
+                        )
+                    type_overrides[param_name] = type_override
+
         @wraps(function)
         def wrapped_function(*args, **kwargs):
             for name, callback in override_callbacks.items():
                 kwargs[name] = callback(*args, **_generate_accepted_kwargs(callback, kwargs))
             return function(*args, **_generate_accepted_kwargs(function, kwargs))
 
+        wrapped_function.__annotations__.update(type_overrides)
         return wrapped_function
 
     return wrapper
