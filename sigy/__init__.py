967f142049a7f1a224a6b4fc1559656b957401e5
Timothy Crosley <timothy.crosley@gmail.com>
2023-01-02 01:34:01 -0800

    Attempt to fix github testing / workflows

--- sigy/__init__.py
+++ sigy/__init__.py
@@ -4,6 +4,7 @@ A library to enable reusing and composing Python function signatures.
 """
 __version__ = "0.0.1"
 
+from sigy import introspect
 from sigy.inject import inject
 
-__all__ = ["inject", "__version__"]
+__all__ = ["inject", "introspect", "__version__"]
