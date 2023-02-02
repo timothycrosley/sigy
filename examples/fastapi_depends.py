ce5920cbbeb1824381a598a9ab7c0019e05280db
Timothy Crosley <timothy.crosley@gmail.com>
2023-02-01 20:32:12 -0800

    Add doc for fast api usage

---
+++ examples/fastapi_depends.py
@@ -0,0 +1,28 @@
+"""sigy can act as a framework agnostic replacement for dependencies
+See: https://fastapi.tiangolo.com/tutorial/dependencies/#dependencies-first-steps
+
+Run with uvicorn fastapi_depends:app --reload
+then go to: http://localhost:8000/docs with your browser.
+"""
+from __future__ import annotations
+
+import sigy
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 100):
+    return {"q": q, "skip": skip, "limit": limit}
+
+
+@app.get("/items/")
+@sigy.inject(commons=common_parameters)
+async def read_items(commons: dict):
+    return commons
+
+
+@app.get("/users/")
+@sigy.inject(commons=common_parameters)
+async def read_users(commons: dict):
+    return commons
