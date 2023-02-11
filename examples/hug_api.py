b47a5eaf17b9e8cd9438f114636045a28bbdb6a3
Timothy Crosley <timothy.crosley@gmail.com>
2023-02-11 11:16:29 -0600

    Add hug example usage

---
+++ examples/hug_api.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from typing import NamedTuple
+
+import hug
+
+import sigy
+
+
+class Article(NamedTuple):
+    id: int
+    created: datetime
+    slug: str
+    text: str = "Article Text"
+    comments: tuple[str, ...] = ("one", "two")
+
+
+def load_article(year: int, month: int, day: int, id: int, slug: str) -> Article:
+    return Article(id=id, created=datetime(year=year, month=month, day=day), slug=slug)
+
+
+@hug.get()
+@sigy.inject(article=load_article)
+def article_content(article: Article) -> str:
+    return article.content
+
+
+@hug.get()
+@sigy.inject(article=load_article)
+def article_comments(article: Article) -> tuple[str, ...]:
+    return article.comments
